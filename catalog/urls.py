f29f0f2a5cf1c4f7f49cda6efb0a0ccaaa2754a3
Artem <tsokalov1994@gmail.com>
2025-12-23 13:46:16 +0300

    madeallpages

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors-list'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Book, Author, BookInstance
+from django.views.generic import ListView, DetailView
 # Create your views here.
+
+class AuthorDetailView(DetailView):
+    model = Author
+    context_object_name = 'author'
+
+class AuthorListView(ListView):
+    model = Author
+    paginate_by = 4
+
+
+class BookDetailView(DetailView):
+        model = Book
+        context_object_name = 'book'
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'books'
+    paginate_by = 3
 def index(request):
-    text_head = "Header of main page"
-    text_body = "Body of main page"
-    context = {'text_head': text_head, 'text_body': text_body}
+    text_head = "You can get e books on this site"
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status=2).count
+    authors = Author.objects
+    num_authors = Author.objects.count()
+    #text_body = "Body of main page"
+    context = {'text_head': text_head, 'books': books,
+               'num_books': num_books, 'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'authors':authors, 'num_authors': num_authors}
     return render(request, 'catalog/index.html', context)
+
+def about(request):
+    text_head = "Data about company"
+    name = "World of books"
+    context = {'text_head': text_head, 'name': name}
+    return render(request, 'catalog/about.html', context)
+
+
+def contact(request):
+    text_head = "Contacts"
+    name = "IP Tsokalov"
+    address = "Serova 4/1"
+    tel = "8-928-338-29-98"
+    email = "tsokalov1994@gmail.com"
+
+    context = {'text_head': text_head, 'name': name, 'address': address, 'tel': tel, 'email': email }
+    return render(request, 'catalog/contact.html', context)
